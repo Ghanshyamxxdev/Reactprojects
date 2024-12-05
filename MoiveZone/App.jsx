073ceb71d3f0dd77adc2211be9c3bf988eaec216
Ghanshyamxxdev <ghanshyamvasita502@gmail.com>
2024-12-05 23:48:53 +0530

    Add files via upload

---
+++ MoiveZone/App.jsx
@@ -0,0 +1,30 @@
+
+
+import Fetch_data from "./Components/Fetch_data";
+// import Form from "./Components/Form";
+import MultipeInputHandling from "./Components/MultipeInputHandling";
+import UseEffect from "./Components/UseEffect";
+import Meal from "./Food_Recipe/Meal";
+import Movies from"./MoiveZone/Movies";
+function App() {
+
+
+  
+  
+
+  return (
+    <>
+      <Movies  />
+    {/* <Fetch_data /> */
+    }
+    {/* <Form /> */}
+
+{/* <MultipeInputHandling /> */}
+{/* <Meal /> */}
+
+    
+    </>
+  )
+}
+
+export default App
